85f229ab18f5ab8dbfc8b7d07537706ccb829df7
Kang-Kyu Lee <kangkyu1111@gmail.com>
2023-03-28 15:52:19 -0700

    Show the player list on phone screen

--- app/src/main/java/com/example/topshotviewer2/MainActivity.kt
+++ app/src/main/java/com/example/topshotviewer2/MainActivity.kt
@@ -5,14 +5,17 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import apolloClient
+import com.apollographql.apollo3.api.ApolloResponse
 import com.example.topshotviewer2.ui.theme.TopShotViewer2Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,32 +23,26 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TopShotViewer2Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                PlayerList()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun PlayerList() {
+    var response: ApolloResponse<PlayerListQuery.Data>? by remember { mutableStateOf(null) }
+    var playerList by remember { mutableStateOf(emptyList<PlayerListQuery.Data1>()) }
     LaunchedEffect(Unit) {
-        val response = apolloClient.query(PlayerListQuery()).execute()
-        Log.d("Greeting", "Success ${response.data}")
+        response = apolloClient.query(PlayerListQuery()).execute()
+        Log.d("PlayerList", "Success ${response?.data}")
+        playerList = response?.data?.allPlayers?.data?.filterNotNull().orEmpty()
     }
-
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TopShotViewer2Theme {
-        Greeting("Android")
+    LazyColumn {
+        items(playerList) { player ->
+            Card {
+                Text(text = player.name.toString())
+            }
+        }
     }
 }
