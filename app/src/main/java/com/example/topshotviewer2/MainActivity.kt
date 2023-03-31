74015057b9334b6d54c28472178de8c4b8eb203f
Kang-Kyu Lee <kangkyu1111@gmail.com>
2023-03-31 01:35:16 -0700

    Styling further

--- app/src/main/java/com/example/topshotviewer2/MainActivity.kt
+++ app/src/main/java/com/example/topshotviewer2/MainActivity.kt
@@ -2,20 +2,23 @@ package com.example.topshotviewer2
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import apolloClient
 import com.apollographql.apollo3.api.ApolloResponse
 import com.example.topshotviewer2.ui.theme.TopShotViewer2Theme
@@ -26,7 +29,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TopShotViewer2Theme {
-                PlayerList()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.White
+                ) {
+                    PlayerList()
+                }
             }
         }
     }
@@ -42,7 +50,7 @@ fun PlayerList() {
         playerList = response?.data?.allPlayers?.data?.filterNotNull().orEmpty()
     }
     LazyColumn {
-        item { Text("Players") }
+//        item { Text("Players", textAlign = TextAlign.Center, fontFamily = FontFamily.Default) }
         items(playerList) { player ->
             PlayerView(player)
         }
@@ -55,34 +63,40 @@ fun PlayerView(player: PlayerListQuery.Data1) {
     val scope = rememberCoroutineScope()
     var response: ApolloResponse<PlayerDetailsQuery.Data>? by remember { mutableStateOf(null) }
     var playerDetails by remember { mutableStateOf(PlayerDetailsQuery.PlayerData(jerseyNumber = null, position = null)) }
-    Card(modifier = Modifier.padding(all = 8.dp)) {
-        Column() {
-            Row {
-                Text(
-                    text = player.name.toString(),
-                    modifier = Modifier.padding(all = 4.dp)
-                )
-                OutlinedButton(onClick = {
-                    detailed = !detailed
-                    if (detailed) {
-                        scope.launch {
-                            response = apolloClient.query(PlayerDetailsQuery(playerId = player.id)).execute()
-                            response?.data?.getPlayerDataWithCurrentStats?.playerData?.apply {
-                                playerDetails = this
+    Surface(
+        color = Color.LightGray,
+        modifier = Modifier.fillMaxWidth().padding(all = 8.dp)
+    ) {
+        Column {
+            Row(modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
+                Column(modifier = Modifier
+                    .weight(1f)
+                    .padding(all = 4.dp)) {
+                    Text(text = player.name.toString(), fontWeight = FontWeight.Bold, fontSize = 21.sp)
+                    if (detailed && response != null && playerDetails != null) {
+                        Text(playerDetails.jerseyNumber?.let { "Jersey Number: $it" } ?: "details not available", fontFamily = FontFamily.Monospace)
+                        Text(playerDetails.position?.let { "Position: $it" } ?: "", fontFamily = FontFamily.Monospace)
+                    }
+                }
+                OutlinedButton(
+                    onClick = {
+                        detailed = !detailed
+                        if (detailed) {
+                            scope.launch {
+                                response = apolloClient.query(PlayerDetailsQuery(playerId = player.id))
+                                    .execute()
+                                response?.data?.getPlayerDataWithCurrentStats?.playerData?.apply {
+                                    playerDetails = this
+                                }
                             }
                         }
-                    }
-                }) {
-                    Text(if (detailed) "Back" else "Details")
-                }
-            }
-            if (detailed) {
-                Row {
-                    if (playerDetails != null && playerDetails.jerseyNumber != null && playerDetails.position != null) {
-                        Text(text = "Jersey: ${playerDetails.jerseyNumber} Position: ${playerDetails.position}")
-                    } else {
-                        Text("player details not available")
-                    }
+                    },
+                    border = BorderStroke(width = 1.dp, color = colorResource(id = R.color.black))
+                ) {
+                    Text(
+                        text = if (detailed) stringResource(R.string.button_back) else stringResource(R.string.button_details),
+                        color = colorResource(id = R.color.black)
+                    )
                 }
             }
         }
