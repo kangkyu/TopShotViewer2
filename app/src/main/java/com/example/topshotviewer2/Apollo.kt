043521cb4b2844c55bede3387cb96d9a907d9596
Kang-Kyu Lee <kangkyu1111@gmail.com>
2023-03-31 01:43:42 -0700

    Set user-agent header

--- app/src/main/java/com/example/topshotviewer2/Apollo.kt
+++ app/src/main/java/com/example/topshotviewer2/Apollo.kt
@@ -1,5 +1,16 @@
 import com.apollographql.apollo3.ApolloClient
+import com.apollographql.apollo3.api.http.HttpRequest
+import com.apollographql.apollo3.api.http.HttpResponse
+import com.apollographql.apollo3.network.http.HttpInterceptor
+import com.apollographql.apollo3.network.http.HttpInterceptorChain
 
 val apolloClient = ApolloClient.Builder()
     .serverUrl("https://public-api.nbatopshot.com/graphql")
+    .addHttpInterceptor(UserAgentInterceptor("kangkyu1111@gmail.com"))
     .build()
+
+private class UserAgentInterceptor(val userAgent: String) : HttpInterceptor {
+    override suspend fun intercept(request: HttpRequest, chain: HttpInterceptorChain): HttpResponse {
+        return chain.proceed(request.newBuilder().addHeader("User-Agent", userAgent).build())
+    }
+}
