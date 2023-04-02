1cb0c1b319b63250c4c09a717d32e3f2ad42e7f7
Kang-Kyu Lee <kangkyu1111@gmail.com>
2023-04-02 01:54:20 -0700

    Add font and more styling

--- app/src/main/java/com/example/topshotviewer2/MainActivity.kt
+++ app/src/main/java/com/example/topshotviewer2/MainActivity.kt
@@ -5,21 +5,19 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import apolloClient
 import com.apollographql.apollo3.api.ApolloResponse
 import com.example.topshotviewer2.ui.theme.TopShotViewer2Theme
@@ -28,58 +26,61 @@ import kotlinx.coroutines.launch
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            TopShotViewer2Theme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = colorResource(R.color.white)
-                ) {
-                    PlayerList()
-                }
-            }
+        setContent { TopShotApp() }
+    }
+}
+
+@Composable
+fun TopShotApp() {
+    TopShotViewer2Theme {
+        Scaffold() { padding ->
+            PlayerList(Modifier.padding(padding))
         }
     }
 }
 
 @Composable
-fun PlayerList() {
+fun PlayerList(modifier: Modifier = Modifier) {
     var responseDetails: ApolloResponse<PlayerDetailsQuery.Data>? by remember { mutableStateOf(null) }
     var response: ApolloResponse<PlayerListQuery.Data>? by remember { mutableStateOf(null) }
     var playerDetails by remember {
         mutableStateOf(PlayerDetailsQuery.PlayerData(jerseyNumber = null, position = null))
     }
-    val scope = rememberCoroutineScope()
     var playerList by rememberSaveable { mutableStateOf(emptyList<PlayerListQuery.Data1>()) }
     LaunchedEffect(Unit) {
         response = apolloClient.query(PlayerListQuery()).execute()
         Log.d("PlayerList", "Success ${response?.data}")
         playerList = response?.data?.allPlayers?.data?.filterNotNull().orEmpty()
     }
-
+    val scope = rememberCoroutineScope()
     val drawerState = rememberDrawerState(DrawerValue.Closed)
     ModalDrawer(
         drawerState = drawerState,
         drawerContent = {
-            Text("Hello, I am a drawer")
-            if (drawerState.isOpen && responseDetails != null && playerDetails != null) {
-                Text(playerDetails.jerseyNumber?.let { "Jersey Number: $it" } ?: "details not available", fontFamily = FontFamily.Monospace)
-                Text(playerDetails.position?.let { "Position: $it" } ?: "", fontFamily = FontFamily.Monospace)
-            }
-            OutlinedButton(
-                onClick = {
-                    scope.launch {
-                        drawerState.close()
+            Column(modifier = Modifier.padding(16.dp)) {
+                if (drawerState.isOpen && responseDetails != null) {
+                    Text(playerDetails.jerseyNumber?.let { "Jersey Number: $it" } ?: "details not available")
+                    Text(playerDetails.position?.let { "Position: $it" } ?: "")
+                }
+                OutlinedButton(
+                    onClick = {
+                        scope.launch { drawerState.close() }
                     }
-                },
-                border = BorderStroke(width = 1.dp, color = colorResource(id = R.color.purple_700))
-            ) {
-                Text(stringResource(R.string.button_back))
+                ) {
+                    Text(stringResource(R.string.button_back), style = MaterialTheme.typography.body2)
+                }
             }
         }
     ) {
-        LazyColumn {
-            items(playerList) { player ->
-                PlayerView(player, onDetailsClick = {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            contentPadding = PaddingValues(vertical = 16.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = modifier
+        ) {
+            items(playerList) {player ->
+                PlayerView(player, modifier = modifier, onDetailsClick = {
                     scope.launch {
                         drawerState.open()
                         responseDetails = apolloClient.query(PlayerDetailsQuery(playerId = player.id)).execute()
@@ -91,31 +92,20 @@ fun PlayerList() {
             }
         }
     }
+
 }
 
 @Composable
-fun PlayerView(player: PlayerListQuery.Data1, onDetailsClick: () -> Unit) {
-
+fun PlayerView(player: PlayerListQuery.Data1, onDetailsClick: () -> Unit, modifier: Modifier = Modifier) {
     Surface(
-        color = colorResource(R.color.white),
-        modifier = Modifier.fillMaxWidth().padding(all = 4.dp)
+        shape = MaterialTheme.shapes.small,
+        modifier = modifier.clickable(onClick = { onDetailsClick() })
     ) {
-        Column {
-            Row(modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
-                Column(modifier = Modifier
-                    .weight(1f)
-                    .padding(all = 4.dp)) {
-                    Text(text = player.name.toString(), fontWeight = FontWeight.Bold, fontSize = 21.sp)
-                }
-                OutlinedButton(
-                    onClick = {
-                        onDetailsClick()
-                    },
-                    border = BorderStroke(width = 1.dp, color = colorResource(id = R.color.purple_700))
-                ) {
-                    Text(stringResource(R.string.button_details))
-                }
-            }
+        Row(modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp), verticalAlignment = Alignment.CenterVertically) {
+            Text(player.name.toString(),
+                modifier = Modifier.padding(all = 4.dp),
+                style = MaterialTheme.typography.h3,
+            )
         }
     }
 }
@@ -124,6 +114,10 @@ fun PlayerView(player: PlayerListQuery.Data1, onDetailsClick: () -> Unit) {
 @Composable
 fun DefaultPreview() {
     TopShotViewer2Theme {
-        PlayerView(PlayerListQuery.Data1(id="1630462", name="Aari McDonald"), onDetailsClick = {})
+        PlayerView(
+            PlayerListQuery.Data1(id="1630462", name="Aari McDonald"),
+            modifier = Modifier.padding(8.dp),
+            onDetailsClick = {}
+        )
     }
 }
--- app/src/main/java/com/example/topshotviewer2/ui/theme/Type.kt
+++ app/src/main/java/com/example/topshotviewer2/ui/theme/Type.kt
@@ -5,24 +5,68 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.Font
+import com.example.topshotviewer2.R
+
+private val fontFamilyKulim = FontFamily(
+    listOf(
+        Font(
+            resId = R.font.kulim_park_regular
+        ),
+        Font(
+            resId = R.font.kulim_park_light,
+            weight = FontWeight.Light
+        )
+    )
+)
+
+private val fontFamilyLato = FontFamily(
+    listOf(
+        Font(
+            resId = R.font.lato_regular
+        ),
+        Font(
+            resId = R.font.lato_bold,
+            weight = FontWeight.Bold
+        )
+    )
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    defaultFontFamily = fontFamilyLato,
+    h1 = TextStyle(
+        fontFamily = fontFamilyKulim,
+        fontWeight = FontWeight.Light,
+        fontSize = 28.sp,
+        letterSpacing = (1.15).sp
+    ),
+    h2 = TextStyle(
+        fontFamily = fontFamilyKulim,
+        fontSize = 15.sp,
+        letterSpacing = (1.15).sp
+    ),
+    h3 = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        letterSpacing = 0.sp
+    ),
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
+        fontSize = 14.sp,
+        letterSpacing = 0.sp
+    ),
+    body2 = TextStyle(
+        fontSize = 12.sp,
+        letterSpacing = 0.sp
+    ),
     button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        letterSpacing = (1.15).sp
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+        fontFamily = fontFamilyKulim,
+        fontSize = 12.sp,
+        letterSpacing = (1.15).sp
+    ),
 )
